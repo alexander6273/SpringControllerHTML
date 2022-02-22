01049dd1cf623f01d4bafa00de7306aed0683327
alexander6273 <alex6273@stud.kea.dk>
2022-02-22 18:30:04 +0100

    Initial commit

---
+++ src/main/java/com/springcontrollerhtml/SpringControllerHtmlApplication.java
@@ -0,0 +1,15 @@
+package com.springcontrollerhtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringControllerHtmlApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringControllerHtmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springcontrollerhtml/SpringControllerHtmlApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springcontrollerhtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringControllerHtmlApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
